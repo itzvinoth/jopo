8191fb407bfde7797dc691ae10dd9e2aa94e535c
vinoth <vinoth.vinothrenga@gmail.com>
2017-07-27 22:47:21 +0530

    project initiated

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+var webpack = require("webpack");
+var path = require("path");
+ 
+var DEV = path.resolve(__dirname, "dev");
+var OUTPUT = path.resolve(__dirname, "output");
+ 
+var config = {
+  entry: DEV + "/index.jsx",
+  output: {
+    path: OUTPUT,
+    filename: "code.js"
+  },
+  module: {
+    loaders: [{
+        include: DEV,
+        loader: "babel-loader",
+    }]
+  }
+};
+ 
+module.exports = config;
