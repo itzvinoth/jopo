295f8a47f39f1f82ab479d5b60b4d27d5f7d5ad2
vinoth <vinoth.vinothrenga@gmail.com>
2017-10-02 11:07:40 +0530

    basic structure implemented with antd and es6

--- dev/components/Cards.js
+++ dev/components/Cards.js
@@ -1,122 +1,82 @@
 import React from 'react';
+import { Card, Radio } from 'antd';
+import './Card.css';
 
-var createReactClass = require('create-react-class');
-const Cards = createReactClass({
-  getInitialState: function() {
-    return ({data: [], requestSent: false,defaultState:[],length: 50,startKey: 1});
-  }, 
+const RadioGroup = Radio.Group;
+export default class Cards extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    // We can't use other names instead of 'state' prop in obj
+    // Because 'state' is an existing property name in React   
+    this.state = {
+      cards: ["card1","card2","card3","card4","card5","card6","card7","card8","card9"],
+      value: 1
+    };
 
-  componentDidMount: function() {
-    window.addEventListener('scroll', this.handleOnScroll);
-
-    this.initFakeData();
-  },
-
-  componentWillUnmount: function() {
-    window.removeEventListener('scroll', this.handleOnScroll);
-  },
-
-  initFakeData: function() {
-    var data = this.createFakeData(this.state.data.length, this.state.length, 1);
-    this.setState({data: data,defaultState:data});
-  },
-
-  createFakeData: function(startKey, counter, incCounter) {
-    console.log(startKey)
-    var i = 0;
-    var data = [];
-    if(startKey%incCounter !== 0) {
-      startKey += startKey%incCounter
-    }
-    for (i = startKey; i < counter; i+=incCounter) {
-      var fakeData = (<div key={i} id={i} className="col-md-4 animate"><div className="data-info"><div className="data-text">Data {i}</div></div></div>);
-      data.push(fakeData);
-    }
-
-    return data;
-  },
-
-  querySearchResult: function() {
-    if (this.state.requestSent) {
-      return;
-    }
-
-    setTimeout(this.doQuery, 2000);
-
-    this.setState({requestSent: true});
-  },
-
-  doQuery: function() {
-        var newlen = this.state.length + 50;
-        console.log(this.state.length,newlen)
-        var fakeData = this.createFakeData(this.state.length, newlen, this.state.startKey);
-        var newData = this.state.data.concat(fakeData);
-        this.setState({data: newData, requestSent: false,length:newlen});
-  },  
-
-  filterdivide: function(value) {
-    if (value === 0) {
-      this.setState({data:this.state.defaultState,length:100,startKey: 1})
-    } else {
-     var newlen = this.state.length + 50;
-     var  data = this.createFakeData(0,newlen, value);
-      this.setState({data:data,startKey:value,length:newlen,startKey:value})
-    }    
-  }, 
-
-  filterdivideby: function(event) {
-    this.filterdivide(parseFloat(event.target.id.replace('f','')));
-  },
-
-  handleOnScroll: function() {
-    var scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
-    var scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
-    var clientHeight = document.documentElement.clientHeight || window.innerHeight;
-    var scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= (scrollHeight-2000);
-
-    if (scrolledToBottom) {
-      this.querySearchResult();
-    }
-  },
-
-  render: function() {
-    return (
-      <div>  
-        
-        <div className="container">
-        <div className="col-md-12">
-            <input id="f3" onClick={this.filterdivideby} type="radio" name="filter" />
-            <label htmlFor="f3">Filter 3</label>
-          <input id="f10" onClick={this.filterdivideby} type="radio" name="filter"/>
-          <label htmlFor="f10">Filter 10</label>
-          <input id="f5" onClick={this.filterdivideby} type="radio" name="filter"/>
-          <label htmlFor="f5">Filter 5</label>
-          <input id="f1" onClick={this.filterdivideby} type="radio" name="filter"/>
-          <label htmlFor="f1">Filter 1</label>
-          <input id="f15" onClick={this.filterdivideby} type="radio" name="filter"/>
-          <label htmlFor="f15">Filter 15</label>
-          <input id="f0" onClick={this.filterdivideby} type="radio" name="filter"/>
-          <label htmlFor="f0">Remove Filter</label>
-          </div>
-          {this.state.data}
-        </div>
-        {(() => {
-          if (this.state.requestSent) {
-            return(
-              <div className="data-loading">
-                <i className="fa fa-refresh fa-spin"></i>
-              </div>
-            );
-          } else {
-            return(
-              <div className="data-loading"></div>
-            );
-          }
-        })()}
-      </div>
-    );
+    this.createListItems = this.createListItems.bind(this);
+    this.handleOnScroll = this.handleOnScroll.bind(this);
+    this.onChange = this.onChange.bind(this);
   }
-});
 
+  componentDidMount() {
+    // We can't directly use setState in here.
+    window.addEventListener("scroll", this.handleOnScroll);
+    this.createListItems();
+  }
 
-export default Cards;
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleOnScroll);
+  }
+
+  newCardCreator() {
+    this.cardsLen= this.state.cards.length;
+    this.newCard = "card"+parseInt(this.cardsLen+1);
+  }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value,
+    });
+  }
+
+  handleOnScroll() {
+    const windowHeight = "innerHeight" in window ? window.innerHeight : document.documentElement.offsetHeight;
+    const body = document.body;
+    const html = document.documentElement;
+    const docHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,  html.scrollHeight, html.offsetHeight);
+    const windowBottom = windowHeight + window.pageYOffset;
+    if (windowBottom >= docHeight) {
+      this.newCardCreator();
+      this.setState({ cards: this.state.cards.concat(this.newCard) })
+    }
+  }
+
+  createListItems() {
+    this.newCardCreator();
+    this.setState({ cards: this.state.cards.concat(this.newCard) })
+  }
+
+  render() {
+    return ( 
+      <div>
+        <button onClick={this.createListItems}> Add </button>
+        <RadioGroup onChange={this.onChange} value={this.state.value}>
+          <Radio value={1}>A</Radio>
+          <Radio value={2}>B</Radio>
+          <Radio value={3}>C</Radio>
+          <Radio value={4}>D</Radio>
+        </RadioGroup>
+        {this.state.cards.map((card, id) => { 
+            return (<div key={id}><Card style={{ width: 240 }} bodyStyle={{ padding: 0 }}>
+                      <div className="custom-card">
+                        <h3>{card}</h3>
+                      </div>
+                    </Card></div>)
+          })}
+        
+      </div>
+    )
+  }
+  
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 var webpack = require("webpack");
 var path = require("path");
- 
+
 var DEV = path.resolve(__dirname, "dev");
 var OUTPUT = path.resolve(__dirname, "output");
- 
+
 var config = {
   entry: DEV + "/index.jsx",
   output: {
@@ -12,11 +12,16 @@ var config = {
   },
   module: {
     loaders: [{
-        include: DEV,
-        loader: "babel-loader",
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      include: DEV,
+      loader: "babel-loader"
+    }, {
+      test: /\.css$/,
+      loader: "style-loader!css-loader"
     }]
   },
   watch: true
 };
- 
+
 module.exports = config;
