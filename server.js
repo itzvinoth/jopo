6820ede260af600e03d6f5f44a318b3050bd5a90
vinoth <vinoth.vinothrenga@gmail.com>
2017-10-10 23:38:05 +0530

    server file modified

--- server.js
+++ server.js
@@ -7,26 +7,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/jopo");
-var nameSchema = new mongoose.Schema({
-    userName: String
-});
-var User = mongoose.model("User", nameSchema);
+mongoose.connect("mongodb://localhost:27017/jopo", {useMongoClient: true});
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/adduser", (req, res) => {
-    var myData = new User(req.body);
-    myData.save()
-        .then(item => {
-            res.send("Name saved to database");
-        })
-        .catch(err => {
-            res.status(400).send("Unable to save to database");
-        });
-});
 
 app.listen(port, () => {
     console.log("Server listening on port " + port);
